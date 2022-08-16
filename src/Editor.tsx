c48d62cb12032b7f5f71aae85e2e3e17fc95940a
m19e <nexus.me7290@gmail.com>
2022-08-16 19:50:15 +0900

    Add minimum editor

---
+++ src/Editor.tsx
@@ -0,0 +1,20 @@
+import type { ComponentProps, FC } from "react";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+
+const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
+    namespace: "LexicalEditor",
+    onError: (error) => console.error(error),
+};
+
+export const Editor: FC = () => {
+    return (
+        <LexicalComposer initialConfig={initialConfig}>
+            <RichTextPlugin
+                contentEditable={<ContentEditable />}
+                placeholder={<div>What's happening?</div>}
+            />
+        </LexicalComposer>
+    );
+};
