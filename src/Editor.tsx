6196f7e2cc367d381bee587ac4b8acb8d4943f22
m19e <nexus.me7290@gmail.com>
2022-08-16 20:05:49 +0900

    Update tab width configuration

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -4,17 +4,17 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 
 const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
-    namespace: "LexicalEditor",
-    onError: (error) => console.error(error),
+  namespace: "LexicalEditor",
+  onError: (error) => console.error(error),
 };
 
 export const Editor: FC = () => {
-    return (
-        <LexicalComposer initialConfig={initialConfig}>
-            <RichTextPlugin
-                contentEditable={<ContentEditable />}
-                placeholder={<div>What's happening?</div>}
-            />
-        </LexicalComposer>
-    );
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>What's happening?</div>}
+      />
+    </LexicalComposer>
+  );
 };
