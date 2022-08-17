9d38cfea5ee4443f218f3a380ed492ba07888984
m19e <nexus.me7290@gmail.com>
2022-08-17 20:50:18 +0900

    Add toolbar plugin

---
+++ src/plugins/ToolbarPlugin.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import type { FC } from "react";
+import { TbH1, TbH2, TbH3 } from "react-icons/all";
+
+import styles from "@/plugins/ToolbarPlugin.module.scss";
+
+const SupportedBlockType = {
+  paragraph: "Paragraph",
+  h1: "Heading 1",
+  h2: "Heading 2",
+  h3: "Heading 3",
+  h4: "Heading 4",
+  h5: "Heading 5",
+  h6: "Heading 6",
+} as const;
+type BlockType = keyof typeof SupportedBlockType;
+
+export const ToolbarPlugin: FC = () => {
+  const [blockType, setBlockType] = useState<BlockType>("paragraph");
+
+  return (
+    <div className={styles.toolbar}>
+      <button
+        type="button"
+        role="checkbox"
+        title={SupportedBlockType["h1"]}
+        aria-label={SupportedBlockType["h1"]}
+        aria-checked={blockType === "h1"}
+      >
+        <TbH1 />
+      </button>
+      <button
+        type="button"
+        role="checkbox"
+        title={SupportedBlockType["h2"]}
+        aria-label={SupportedBlockType["h2"]}
+        aria-checked={blockType === "h2"}
+      >
+        <TbH2 />
+      </button>
+      <button
+        type="button"
+        role="checkbox"
+        title={SupportedBlockType["h3"]}
+        aria-label={SupportedBlockType["h3"]}
+        aria-checked={blockType === "h3"}
+      >
+        <TbH3 />
+      </button>
+    </div>
+  );
+};
