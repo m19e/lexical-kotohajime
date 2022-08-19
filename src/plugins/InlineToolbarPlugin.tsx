8432883ef03480aa6ef1081f7ae7a714d7b65a11
m19e <nexus.me7290@gmail.com>
2022-08-19 18:33:43 +0900

    Set inline-toolbar buttons

--- src/plugins/InlineToolbarPlugin.tsx
+++ src/plugins/InlineToolbarPlugin.tsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { FORMAT_TEXT_COMMAND } from "lexical";
+import {
+  MdCode,
+  MdFormatBold,
+  MdFormatItalic,
+  MdFormatStrikethrough,
+  MdFormatUnderlined,
+  MdSubscript,
+  MdSuperscript,
+} from "react-icons/all";
 
 import styles from "@/plugins/InlineToolbarPlugin.module.scss";
 
@@ -14,5 +24,75 @@ export const InlineToolbarPlugin = () => {
   const [isSubscript, setIsSubscript] = useState(false);
   const [isSuperscript, setIsSuperscript] = useState(false);
 
-  return <div className={styles.inlineToolbar}></div>;
+  return (
+    <div className={styles.inlineToolbar}>
+      <button
+        type="button"
+        aria-label="format bold"
+        role="checkbox"
+        aria-checked={isBold}
+        onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold")}
+      >
+        <MdFormatBold />
+      </button>
+      <button
+        type="button"
+        aria-label="format underline"
+        role="checkbox"
+        aria-checked={isUnderline}
+        onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline")}
+      >
+        <MdFormatUnderlined />
+      </button>
+      <button
+        type="button"
+        aria-label="format strikethrough"
+        role="checkbox"
+        aria-checked={isStrikethrough}
+        onClick={() =>
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "strikethrough")
+        }
+      >
+        <MdFormatStrikethrough />
+      </button>
+      <button
+        type="button"
+        aria-label="format italic"
+        role="checkbox"
+        aria-checked={isItalic}
+        onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic")}
+      >
+        <MdFormatItalic />
+      </button>
+      <button
+        type="button"
+        aria-label="format code"
+        role="checkbox"
+        aria-checked={isCode}
+        onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, "code")}
+      >
+        <MdCode />
+      </button>
+      <button
+        type="button"
+        aria-label="format subscript"
+        role="checkbox"
+        aria-checked={isSubscript}
+        onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, "subscript")}
+      >
+        <MdSubscript />
+      </button>
+      <button
+        type="button"
+        aria-label="format superscript"
+        role="checkbox"
+        aria-checked={isSuperscript}
+        onClick={() =>
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "superscript")
+        }
+      >
+        <MdSuperscript />
+      </button>
+    </div>
+  );
 };
